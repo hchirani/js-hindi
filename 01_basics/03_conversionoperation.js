78c875e6756cac7ebbd81b160e8902e5c8a62824
hchirani <140970479+hchirani@users.noreply.github.com>
2023-07-31 10:04:04 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,22 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn ="hiren"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+// "" => false
+// "hiren" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber); 
