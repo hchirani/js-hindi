18c0a895c6e1d0561ebc02c4004eb2ef501fed5c
hchirani <140970479+hchirani@users.noreply.github.com>
2023-07-31 05:59:22 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,3 +2,21 @@
  
 console.log((3+3), 'hitesh');
 
+// code readability should be high
+
+let name = "hiren"
+let age = 23
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigint => use in big-site
+// string => "preferd in dobble squade"
+// boolean => true/false
+// null =>  standalone value
+// undefined => value not assigned 
+// symbol => unique item defined
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
