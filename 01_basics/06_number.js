44703aed25ca9628e3ea74ec2098b403912db80b
hchirani <140970479+hchirani@users.noreply.github.com>
2023-08-01 05:03:54 +0000

    number done

---
+++ 01_basics/06_number.js
@@ -0,0 +1,31 @@
+// const score = 300
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.4568
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString());
+
+//++++++++++++++++++++++++++++++ maths +++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.sqrt(50));
+// console.log(Math.random());
+// console.log((Math.random()*10));
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random()* (max - min + 1))) + min);
