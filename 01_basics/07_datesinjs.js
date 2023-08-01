8fec39c7f260a4fe6b11973f4a99af6f9145099e
hchirani <140970479+hchirani@users.noreply.github.com>
2023-08-01 05:52:31 +0000

    done

--- 01_basics/07_datesinjs.js
+++ 01_basics/07_datesinjs.js
@@ -4,12 +4,11 @@ let myDate = new Date()
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+//                          console.log(typeof myDate);
 
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
+// let myCreatedDate = new Date("08-01-2023")
 // console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
@@ -19,13 +18,12 @@ let myTimeStamp = Date.now()
 // console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
 console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
 
 newDate.toLocaleString('default', {
     weekday: "long",
-    
 })
