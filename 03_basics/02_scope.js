4ee21d5b9f2b2296efc7fb96383ed9f2a799d8ec
hchirani <140970479+hchirani@users.noreply.github.com>
2023-08-02 03:51:15 +0000

    done

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,31 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
