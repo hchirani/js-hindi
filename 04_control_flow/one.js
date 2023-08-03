44eca5a9d7c14837b8ec3b35e82c13c8abbad96d
hchirani <140970479+hchirani@users.noreply.github.com>
2023-08-03 05:47:00 +0000

    done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,11 @@
+// if
+
+const isUserloggedin = true
+const temperature = 41
+
+// if (temperature === 41) {
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("temperature is greater than 50");
+// }
