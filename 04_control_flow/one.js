1ca6c4db642d199450ece101a23dc45c82768caa
hchirani <140970479+hchirani@users.noreply.github.com>
2023-08-03 07:27:51 +0000

    done

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,7 +1,7 @@
 // if
 
-const isUserloggedin = true
-const temperature = 41
+// const isUserloggedin = true
+// const temperature = 41
 
 // if (temperature === 41) {
 //     console.log("less than 50");
@@ -9,3 +9,40 @@ const temperature = 41
 // else{
 //     console.log("temperature is greater than 50");
 // }
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 700) {
+//     console.log("less than 700");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// }
+// else {
+//     console.log("less than 1050");
+// }
+    
+const userLoggedin = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedin && debitCard && 2==4) {
+    // console.log("allow to buy cource");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
