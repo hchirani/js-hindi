387b2b91d3f4116a9def3581cc303ba11e2e7f59
hchirani <140970479+hchirani@users.noreply.github.com>
2023-08-04 10:25:44 +0000

    done

---
+++ 05_interation/five.js
@@ -0,0 +1,9 @@
+const coading = ["js", "ruby", "cpp", "python", "SQL"]
+
+// coading.forEach( function (para) {
+//     console.log(para);
+// } )
+
+coading.forEach ( (item) => {
+    console.log(item);
+} )
